4721b6b594ce23d14d38efd18e280964f0fb80b3
Miksin <ppy951@gmail.com>
2016-11-15 13:58:22 +0800

    9: js (revised)

--- 9_PalindromeNumber/9_v1.js
+++ 9_PalindromeNumber/9_v1.js
@@ -4,5 +4,5 @@
  */
 var isPalindrome = function(x) {
     var reverseInt = Number(x.toString().split("").reverse().join(""))
-    return !isNaN(reverseInt) && reverseInt == x
+    return reverseInt == x
 };
